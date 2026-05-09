08c6a552550410eb780a6352c85d3a4ae5de377e
Dhrumil-Kharadi <dhumil05@gmail.com>
2026-05-09 23:57:43 +0530

    Delete Animation

--- Frontend/src/components/animation/index.js
+++
@@ -1,8 +0,0 @@
-// ============================================================
-// FILE: components/animation/index.js
-// Barrel export for all animation components.
-// Import from '@/components/animation' for clean imports.
-// ============================================================
-
-export { default as ParticleCanvas } from './ParticleCanvas';
-export { default as ScrollReveal   } from './ScrollReveal';
